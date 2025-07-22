41090dd1ae2b724efda30ff1e638b094a6e37b31
Marc-André Legault <marc-andre.legault.1@umontreal.ca>
2025-07-22 09:57:00 -0400

    Added executables to run the docker image.

---
+++ staging/run_workshop_environment_macOS_linux.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+local_dir="${HOME}/STAGE2025_workshop_theme2"
+mkdir -p $local_dir
+
+docker run -ti --rm \
+    -p 8888:8888 \
+    -v $local_dir:/workshop/local \
+    ghcr.io/pgx-ml-lab/stage2025_theme2:latest
+
+echo "Exiting!"
---
+++ staging/run_workshop_environment_windows.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set local_dir=%USERPROFILE%\STAGE2025_workshop_theme2
+if not exist "%local_dir%" mkdir "%local_dir%"
+
+docker run -ti --rm ^
+    -p 8888:8888 ^
+    -v "%local_dir%":/workshop/local ^
+    ghcr.io/pgx-ml-lab/stage2025_theme2:latest
+
+echo Exiting!
