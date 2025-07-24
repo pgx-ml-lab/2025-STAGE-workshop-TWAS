4a0134eaf7d50ba4063cca9da3ea2aa981d99bfe
Marc-André Legault <legaultmarc@gmail.com>
2025-07-23 20:55:42 -0400

    Fixed tty bug

--- run_workshop_environment_macOS_linux.sh
+++ run_workshop_environment_macOS_linux.sh
@@ -3,7 +3,7 @@
 local_dir="${HOME}/STAGE2025_workshop_theme2"
 mkdir -p $local_dir
 
-docker run -ti --rm \
+docker run -i --rm \
     -p 8888:8888 \
     -v $local_dir:/workshop/local \
     ghcr.io/pgx-ml-lab/stage2025_theme2:latest
